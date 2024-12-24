82f515db9f2682d12d30369c33f682355c349339
Nishanth <nishanthp03032005@gmail.com>
2024-12-24 20:49:06 +0530

    Update Level 16.py

--- Level 16.py
+++ Level 16.py
@@ -31,3 +31,33 @@ Code:
     print("Yes")
  else:
     print("No")
+
+Write a program that display equal positive and negative numbers for a given number
+Zero should not be displayed
+For example:
+Input	
+6
+Result
+3 2 1 -1 -2 -3
+10
+Result
+5 4 3 2 1 -1 -2 -3 -4 -5
+
+Code:
+#include<stdio.h>
+int main(){
+    int num,i;
+    scanf("%d",&num);
+    if (num == 0){
+        printf("invalid");
+    }
+    else{
+        for(i=num/2;i>=1;i--){
+            printf("%d ",i);
+        }
+        for(i=-1;i>= -num/2;i--){
+            printf("%d ",i);
+        } 
+        printf("\n");
+    }
+}
