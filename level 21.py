a26fa76532cc3811a2f16c37e973cf9a90c2e6b4
Nishanth <nishanthp03032005@gmail.com>
2025-01-06 20:44:39 +0530

    Create level 21.py

---
+++ level 21.py
@@ -0,0 +1,14 @@
+Write a program to find the first digit in a given number
+For example:
+Input	Result
+30    3
+4056  4
+Code:
+  print(input()[0])
+
+Write a program to count the number of digits in a given number
+For example:
+Input	Result
+7896  4
+Code:
+  print(len(input()))
