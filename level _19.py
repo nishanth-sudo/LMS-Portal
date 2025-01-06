46c128f23113eed966a650e1d01b83c26e1349c2
Nishanth <nishanthp03032005@gmail.com>
2025-01-06 20:38:20 +0530

    Create level _19.py

---
+++ level _19.py
@@ -0,0 +1,25 @@
+Write a program to print the following pattern:
+11
+12 13
+14 15 16
+Code:
+  c=11
+  for i in range(1,int(input())+1):
+      for j in range(i):
+          print(c,end=' ')
+          c+=1
+      print()
+
+1
+1 2 
+1 2 3
+Code:
+for i in range(1,int(input())+1):
+    print(" ".join(str(j) for j in range(1,i+1)))
+
+1
+2 2
+3 3 3
+Code:
+  for i in range(1,int(input())+1):
+      print((str(i)+' ')*i)
