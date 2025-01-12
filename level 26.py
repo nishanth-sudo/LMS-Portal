a9996394296bb08f57202436cb4bd46b297ab32d
Nishanth <nishanthp03032005@gmail.com>
2025-01-12 12:47:50 +0530

    Update level 26.py

--- level 26.py
+++ level 26.py
@@ -12,3 +12,39 @@ Code:
       r=list(map(int,input().split()))
       m.append(r)
   print(*[sum(c) for c in zip(*m)])
+
+program to print add and subtraction of two 2d matrices
+input
+2 2
+2 2
+
+1 2 
+3 4
+
+4 5 
+6 7
+
+Code:
+  r1,c1=map(int,input().split())
+  r2,c2=map(int,input().split())
+  if r1<0 and c1<0:
+      print("Row and column size should not be negative")
+  elif r1!=c1:
+      print("Row and column size should be same for 2 matrices")
+  else:
+      m1=[]
+      for i in range(r1):
+          e1=list(map(int,input().split()))
+          m1.append(e1)
+      m2=[]
+      for i in range(r2):
+          e2=list(map(int,input().split()))
+          m2.append(e2)
+      add=[[m1[i][j]+m2[i][j] for j in range(c1)] for i in range(r1)]
+      sub=[[m1[i][j]-m2[i][j] for j in range(c1)] for i in range(r1)]
+      print("Addition:")
+      for i in add:
+          print(*i)
+      print("Subtraction:")
+      for j in sub:
+          print(*j)
