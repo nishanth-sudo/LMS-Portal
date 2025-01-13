e7f3d98f03008786657b9d0085b8d0e767d79b08
Nishanth <nishanthp03032005@gmail.com>
2025-01-13 20:29:27 +0530

    Update level 26.py

--- level 26.py
+++ level 26.py
@@ -48,3 +48,60 @@ Code:
       print("Subtraction:")
       for j in sub:
           print(*j)
+
+find primary and secondary diagonal elements:\
+input
+2
+2
+1
+2
+3
+4
+
+Result
+Primary diagonal elements:
+1
+4
+Secondary diagonal elements:
+2
+3
+Code:
+  r=int(input())
+  c=int(input())
+  if r!=c:
+      print("Row and column size should be same")
+  elif r<0 and c<0:
+      print("Row and column size should not be negative")
+  else:
+      m=[]
+      for i in range(r):
+          e=[]
+          for j in range(c):
+              e.append(int(input()))
+          m.append(e)
+      print("Primary diagonal elements:")
+      for i in range(r):
+          print(m[i][i])
+      print("Secondary diagonal elements:")
+      for i in range(r):
+          print(m[i][r-i-1])
+
+Program to find sum of right diagonal elements in matrix
+input:
+2
+15 48
+10 37
+Result
+52
+Code:
+  r=int(input())
+  m=[]
+  for i in range(r):
+      e=list(map(int,input().split()))
+      m.append(e)
+  s=0
+  for i in range(r):
+      for j in range(r):
+          if i==j:
+              s=s+m[i][j]
+  print("Addition of the right Diagonal elements is:",s)
