5a6fab281ef716aa73c850fd435b5b6f3d522b12
Nishanth <nishanthp03032005@gmail.com>
2025-01-13 21:34:31 +0530

    Update Level18.py

--- Level18.py
+++ Level18.py
@@ -23,3 +23,19 @@ Code:
   n=int(input())
   for i in range(1,n+1):
       print('1 '*i)(str
+input
+3
+result
+*
+# #
+* * *
+Code:
+  n=int(input())
+  if n<0:
+      print("Invalid input")
+  else:
+      for i in range(1,n+1):
+          if i%2!=0:
+              print("* "*i)
+          else:
+              print("# "*i)
