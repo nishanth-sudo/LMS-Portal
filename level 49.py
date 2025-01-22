fe74839c356e357ee4036deda53a69bcfc3e06ce
Nishanth <nishanthp03032005@gmail.com>
2025-01-22 20:20:21 +0530

    Update level 49.py

--- level 49.py
+++ level 49.py
@@ -51,10 +51,17 @@ result
   * *
    *
 Code:
-   *
-  * *
- *   *
-*     *
- *   *
-  * *
-   *
+n=int(input())
+for i in range(1,n+1):
+    print(" "*(n-i),end='')
+    if i==1:
+        print("*")
+    else:
+        print("*"+" "*(2*(i-1)-1)+"*")
+        
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end='')
+    if i==1:
+        print("*")
+    else:
+        print("*"+" "*(2*(i-1)-1)+"*")
